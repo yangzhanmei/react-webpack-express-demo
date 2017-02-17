fff0bd14e454290a25d562f34b66a540cbbad0f8
yangzhanmei <1724617718@qq.com>
2017-02-17 18:37:17 +0800

    correct the error of path

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: path.resolve(__dirname, './public/main.js'),
     output: {
-        path: path.resolve(__dirname, './out'),
+        path: path.resolve(__dirname, './public/out'),
         filename: 'bundle.js'
     },
     module: {
